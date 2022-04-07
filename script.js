987885b9d4bd8428124654e9f843e4d612b0745d
nerdlekar <nerdlekar@gmail.com>
2022-04-07 17:13:41 +0530

    added more functionality

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-const ROWS = 10;
-const screen = document.querySelector(".screen");
-const goButton = document.querySelector(".create-button")
-const clearButton = document.querySelector(".clear")
+let screenSize = 10
+const screen = document.querySelector('.screen')
+const goButton = document.querySelector('.create-button')
+const clearButton = document.querySelector('.clear')
 
-function clearGrid(){
-  screen.innerHTML="";
+function clearGrid () {
+  screen.innerHTML = ''
 }
 
-function createGrid(){
-  // screen.style.gridTemplateColumns = `repeat(${ROWS},1fr)`
-  // screen.style.gridTemplateRows = `repeat(${ROWS},1fr)`
-  
-  for (let i=0; i < ROWS*ROWS ; i++){
-  const pixel = document.createElement('div');
-    pixel.classList.add("pixel")
-    pixel.addEventListener('mouseover', console.log("mouseover"))
-    pixel.addEventListener('mousedown', console.log("mousedown"))
+function createGrid () {
+  screen.style.gridTemplateColumns = `repeat(${screenSize},1fr)`
+  screen.style.gridTemplateRows = `repeat(${screenSize},1fr)`
+
+  for (let i = 0; i < screenSize ** 2; i++) {
+    const pixel = document.createElement('div')
+    pixel.classList.add('pixel')
+    // pixel.addEventListener('mouseover', console.log("mouseover"))
+    // pixel.addEventListener('mousedown', console.log("mousedown"))
+    pixel.style.borderStyle = 'solid'
     screen.appendChild(pixel)
   }
 }
 
-
-goButton.addEventListener("click", createGrid)
-clearButton.addEventListener("click", clearGrid)
-
-
+goButton.addEventListener('click', createGrid)
+clearButton.addEventListener('click', clearGrid)
