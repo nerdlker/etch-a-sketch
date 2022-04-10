e35abcd7a3950c5cf9c9bc91492febaab059a839
nerdlekar <nerdlekar@gmail.com>
2022-04-10 13:09:55 +0530

    Added mouseover functionality

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const screen = document.querySelector('.screen')
 const goButton = document.querySelector('.create-button')
 const clearButton = document.querySelector('.clear')
 
+
 function clearGrid () {
   screen.innerHTML = ''
+}
+
+function draw(){
+this.style.backgroundColor="black";
 }
 
 function createGrid () {
@@ -14,8 +19,8 @@ function createGrid () {
   for (let i = 0; i < screenSize ** 2; i++) {
     const pixel = document.createElement('div')
     pixel.classList.add('pixel')
-    // pixel.addEventListener('mouseover', console.log("mouseover"))
-    // pixel.addEventListener('mousedown', console.log("mousedown"))
+    pixel.addEventListener('mouseover',draw)
+    // pixel.addEventListener('mousedown', draw)
     pixel.style.borderStyle = 'solid'
     screen.appendChild(pixel)
   }
