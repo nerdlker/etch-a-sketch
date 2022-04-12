5d724772d1a74797037637314f905865f41dc897
nerdlekar <nerdlekar@gmail.com>
2022-04-12 10:51:40 +0530

    show grid now works as intended

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-let screenSize = 10
+let screenSize = 16;
 const screen = document.querySelector('.screen')
 const goButton = document.querySelector('.create-button')
 const clearButton = document.querySelector('.clear')
 const blackButton = document.querySelector('.black-button')
-const rainbowButton = document.querySelector('.rainbow-button')
-const defaultColor = "grey"
-let currentColor = defaultColor
-let newColor = currentColor
+const rainbowButton = document.querySelector('.rainbow-button');
+const colorButton = document.querySelector('.color-button')
+const defaultColor = "grey";
+let currentColor = defaultColor;
+let newColor = currentColor;
+var slider = document.getElementById("myRange");
+let drawPixel = true;
+const cb = document.querySelector("#checkmark")
 
+
+
+
+
+var output = document.getElementById("slider-value");
+output.innerHTML = `Grid Size: 16 x 16`;
+slider.oninput = function() {
+  output.textContent = `Grid Size: ${this.value} x ${this.value}`;
+  
+}
 function clearGrid () {
   screen.innerHTML = ''
 }
+
 
 /// NOTE : DRAW will require and if else statement to determin a 'new color' which will then test for the 'current color'.
 
@@ -31,18 +46,23 @@ this.style.backgroundColor = currentColor;
 }
 
 function createGrid () {
+  clearGrid();
+  screenSize = slider.value;
   screen.style.gridTemplateColumns = `repeat(${screenSize},1fr)`
   screen.style.gridTemplateRows = `repeat(${screenSize},1fr)`
 
   for (let i = 0; i < screenSize ** 2; i++) {
     const pixel = document.createElement('div')
     pixel.classList.add('pixel')
+    if (cb.checked == true)
+    {pixel.classList.add("showscreen")}
+    
     pixel.addEventListener('mouseover',draw)
     // pixel.addEventListener('mousedown', draw)
-    pixel.style.borderStyle = 'solid'
     screen.appendChild(pixel)
     currentColor = defaultColor;
   }
+  
 }
 
 function drawBlack(){
@@ -55,8 +75,31 @@ function drawRainbow(){
   newColor= "rainbow"
 
 }
+function gridToggle(){ //toggle grid
+  elements = screen.querySelectorAll(".pixel");
+  for (let i = 0; i < elements.length; i++) {
+  let show = elements[i].classList.toggle("showscreen")
+}}
+
+
+
+createGrid()
+cb.addEventListener('change', gridToggle)
 
 goButton.addEventListener('click', createGrid)
 clearButton.addEventListener('click', clearGrid)
 blackButton.addEventListener('click', drawBlack)
 rainbowButton.addEventListener('click', drawRainbow)
+// colorButton.addEventListener("click", doThis)
+
+
+/* 
+todo :
+1. Add toggle for 'show grid' [DONE]
+2. wire up toggle to display/hide grid [DONE]
+3. add fonts
+4. decorate screen box better
+
+add keyboard shortcurts 
+
+*/
